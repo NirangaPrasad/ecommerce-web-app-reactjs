7c7996f5a135406f6ddb9be25cd2665a472821c5
niranga <nirangaalgama877@gmail.com>
2023-03-05 14:07:56 +0530

    Create products page

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            productName:"Nike slim shirt",
+            slug:"nike-slim-shirt",
+            category:"shirt",
+            image:"./img/p1.png",
+            price:120,
+            countInStock:10,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            productName:"Adidas Fit Shirt",
+            slug:"adidas-fit-shirt",
+            category:"shirt",
+            image:"./img/p2.png",
+            price:250,
+            countInStock:20,
+            brand:"Adidas",
+            rating:4.0,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            productName:"Nike slim pant",
+            slug:"nike-slim-pant",
+            category:"pant",
+            image:"./img/p3.png",
+            price:100,
+            countInStock:15,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:14,
+            description:"high quality product"
+        },
+        {
+            productName:"Adidas Fit pant",
+            slug:"adidas-fit-pant",
+            category:"pant",
+            image:"./img/p4.png",
+            price:140,
+            countInStock:20,
+            brand:"Adidas",
+            rating:4.8,
+            numReviews:18,
+            description:"high quality product"
+        },
+    ]
+}
+
+export default data
