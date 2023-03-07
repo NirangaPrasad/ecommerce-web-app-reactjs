eebd59d974a32a5fe26098a51b73eda1cd2b63ae
niranga <nirangaalgama877@gmail.com>
2023-03-07 20:56:10 +0530

    Add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import ProductScreen from "./screens/ProductScreen"
-
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="/">amazona</Navbar.Brand>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />}/>
-            <Route path="/product/:slug" element={<ProductScreen/>}/>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,81 @@
-import data from "../data";
 import "../App.css";
 import { Link } from "react-router-dom";
+import { useEffect, useReducer} from "react";
+
 
 const HomeScreen = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return {
+          ...state,
+          loading: true,
+        };
+      case "PRODUCTS_RECEIVED":
+        return {
+          ...state,
+          loading: false,
+          products:action.payload
+        };
+      case "ERROR":
+        return {
+          ...state,
+          loading: false,
+          err: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  const [{ products, loading, err }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    err: "",
+  });
+  // const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_REQUEST" });
+    const fetchData = async () => {
+      const response = await fetch("/api/products", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const data = await response.json();
+      dispatch({ type: "PRODUCTS_RECEIVED", payload: data });
+    };
+    try {
+      fetchData();
+    } catch (e) {
+      dispatch({ type: "ERROR", payload: e });
+    }
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product, index) => (
-          <div className="product" key={index}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.productName} />
-            </Link>
-
-            <div className="proudctInfo">
-              <p>{product.productName}</p>
-              <p>${product.price}</p>
-              <button>Add to cart</button>
+        {loading ? (
+          <div>Loading....</div>
+        ) : err ? (
+          <div>{err}</div>
+        ) : (
+          products?.map((product, index) => (
+            <div className="product" key={index}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.productName} />
+              </Link>
+              <div className="proudctInfo">
+                <p>{product.productName}</p>
+                {}
+                <p>${product.price}</p>
+                <button>Add to cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
